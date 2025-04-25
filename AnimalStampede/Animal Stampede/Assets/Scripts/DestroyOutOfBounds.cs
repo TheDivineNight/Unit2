642fdffa1d1f83065578e0c3b67e0758be56ba99
TheDivineNight <bedoyad@roverkids.org>
2025-04-25 14:16:53 -0400

    Lesson 2.4 Done

--- AnimalStampede/Animal Stampede/Assets/Scripts/DestroyOutOfBounds.cs
+++ AnimalStampede/Animal Stampede/Assets/Scripts/DestroyOutOfBounds.cs
@@ -15,11 +15,13 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if an object goes past the player's view ingame, destroy object
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         } else if(transform.position.z < lowerBound)
         {
+            Debug.Log("GameOver");
             Destroy(gameObject);
         }
     }
